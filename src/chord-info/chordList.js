fa78bcaf97e2701b1540d34c9a796814e3a6c690
nathan sepulveda <nathans.composer@gmail.com>
2019-12-26 16:45:06 -0800

    first commit

---
+++ src/chord-info/chordList.js
@@ -0,0 +1,126 @@
+let CMaj = {
+    type: "Cmaj",
+    name: "Cmaj",
+    notes: ["C3", "E3", "G3"]
+  };
+  
+  let CMaj_firstInverion = {
+    type: "Cmaj",
+    name: "Cmaj First Inversion",
+    notes: ["E3", "G3", "C4"]
+  };
+  
+  let CMaj_secondInverion = {
+    type: "Cmaj",
+    name: "Cmaj Second Inversion",
+    notes: ["G3", "C4", "E4"]
+  };
+  
+  let DMaj = {
+    type: "Dmaj",
+    name: "Dmaj",
+    notes: ["D3", "F#3", "A3"]
+  };
+  
+  let DMaj_firstInverion = {
+    type: "Dmaj",
+    name: "Dmaj First Inversion",
+    notes: ["F#3", "A3", "D4"]
+  };
+  
+  let DMaj_secondInverion = {
+    type: "Dmaj",
+    name: "Dmaj Second Inversion",
+    notes: ["A3", "D4", "F#4"]
+  };
+  
+  let Emin = {
+    type: "Emin",
+    name: "Emin",
+    notes: ["E3", "G3", "B3"]
+  };
+  let Emin_firstInversion = {
+    type: "Emin",
+    name: "Emin First Inversion",
+    notes: ["G3", "B3", "E4"]
+  };
+  let Emin_secondInversion = {
+    type: "Emin",
+    name: "Emin Second Inversion",
+    notes: ["B3", "E4", "G4"]
+  };
+  
+  let Emaj = {
+    type: "Emaj",
+    name: "Emaj",
+    notes: ["E3", "G#3", "B3"]
+  };
+  let Emaj_firstInversion = {
+    type: "Emaj",
+    name: "Emaj First Inversion",
+    notes: ["G#3", "B3", "E4"]
+  };
+  let Emaj_secondInversion = {
+    type: "Emaj",
+    name: "Emaj Second Inversion",
+    notes: ["B3", "E4", "G#4"]
+  };
+  
+  let Fmaj_secondInversion = {
+    type: "Fmaj",
+    name: "Fmaj First Inversion",
+    notes: ["C3", "F3", "A3"]
+  };
+  
+  let Fmaj = {
+    type: "Fmaj",
+    name: "Fmaj",
+    notes: ["F3", "A3", "C4"]
+  };
+  
+  let Fmaj_firstInversion = {
+    type: "Fmaj",
+    name: "Fmaj First Inversion",
+    notes: ["A3", "C4", "F4"]
+  };
+  
+  let Gmaj_firstInversion = {
+    type: "Gmaj",
+    name: "Gmaj First Inversion",
+    notes: ["B3", "D4", "G4"]
+  };
+  
+  let Gmaj = {
+    type: "Gmaj",
+    name: "Gmaj",
+    notes: ["G3", "B3", "D4"]
+  };
+  
+  let Gmaj_secondInversion = {
+    type: "Gmaj",
+    name: "Gmaj Second Inversion",
+    notes: ["D3", "G3", "B3"]
+  };
+  
+  let chordList = [
+    CMaj,
+    CMaj_firstInverion,
+    Fmaj,
+    Fmaj_firstInversion,
+    Fmaj_secondInversion,
+    Emin,
+    Emin_firstInversion,
+    Emin_secondInversion,
+    Emaj,
+    Emaj_firstInversion,
+    Emaj_secondInversion,
+    Gmaj,
+    Gmaj_firstInversion,
+    Gmaj_secondInversion,
+    CMaj_secondInverion,
+    DMaj,
+    DMaj_firstInverion,
+    DMaj_secondInverion
+  ];
+
+export default chordList
